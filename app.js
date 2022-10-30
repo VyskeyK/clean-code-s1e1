edeac45af6f8330efdff73a2ee0381a83155b525
VyskeyK <robintyan96@gmail.com>
2022-10-30 17:35:43 +0300

    fix: add class for delete button image at app.js

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="task__button task__button_delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = "button__image";
     deleteButton.appendChild(deleteButtonImg);
 
 
